8c8d74a3fc5d465ea108b72bc77f6c6ec6f03656
Arctis Fireblight <6182060+Arctis-Fireblight@users.noreply.github.com>
2026-06-04 21:21:43 -0500

    Add Redot logo assets and update callout styles

---
+++ Redot-Documentation/Redot-Documentation/IconConstants.cs
@@ -0,0 +1,10 @@
+namespace Redot_Documentation;
+
+/// <summary>
+/// Static class used for storing fixed icon paths.
+/// </summary>
+public static class IconConstants
+{
+    public static string RedotIcon => "/Icons/redot_logo.svg";
+    public static string TipsIcon => "/Icons/Font-Awsome/regular/lightbulb.svg";
+}
